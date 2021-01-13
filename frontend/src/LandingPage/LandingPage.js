68aa0d811c9d39e28270a447ad86ea719b966a39
Beatrice Tam <beatrice.6293@gmail.com>
2021-01-13 17:10:04 -0500

    Formatted landing page components and added logo files
    Added logo svg files to LandingPage folder, changed Log in button shape, added welcome message and main logo

--- frontend/src/LandingPage/LandingPage.js
+++ frontend/src/LandingPage/LandingPage.js
@@ -1,16 +1,19 @@
 import './LandingPage.css';
+import logo from './overlap large.svg';
+
 
 //Home page component
 //Takes loggedIn boolean as a prop
 const LandingPage = (props) => {
     return (
         <div className="landing-root d-flex flex-column justify-content-center align-items-center">
+            <img src={logo} className="large-logo" alt="logo" />
+            <h2 className="white"> Welcome to <span className="purple">overlap</span>. <br></br>Make listening with friends easy.
+            </h2>
             <a href={process.env.REACT_APP_BACKEND_URL} className="btn btn-sm login-button">
-                Login with Spotify
+                Log in with Spotify
             </a>
-            <h1 className="white">
-                hello
-            </h1>
+           
         </div>
     );
 }
