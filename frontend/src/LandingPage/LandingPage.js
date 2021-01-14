b0f66012f02dcad580a4d49c444167eb924d782b
Danica <danicadickinson@gmail.com>
2021-01-14 09:00:42 -0800

    Simple Text Navbar
    Very simple navbar fixed to top

--- frontend/src/LandingPage/LandingPage.js
+++ frontend/src/LandingPage/LandingPage.js
@@ -1,3 +1,4 @@
+import Navbar from '../Navbar/Navbar';
 import './LandingPage.css';
 
 //Home page component
@@ -5,12 +6,13 @@ import './LandingPage.css';
 const LandingPage = (props) => {
     return (
         <div className="landing-root d-flex flex-column justify-content-center align-items-center">
-            <a href="http://localhost:8888/" className="btn btn-sm login-button">
+            <a href={process.env.REACT_APP_BACKEND_URL} className="btn btn-sm login-button">
                 Login with Spotify
             </a>
             <h1 className="white">
                 hello
             </h1>
+            <Navbar></Navbar>
         </div>
     );
 }
---
+++ frontend/src/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+const Navbar = (props) => {
+    return (
+        <nav class="navbar fixed-top navbar-light bg-light">
+            <span class="navbar-text">
+                Overlap
+            </span>
+        </nav>
+    );
+}
+
+export default Navbar;
