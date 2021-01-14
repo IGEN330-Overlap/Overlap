d744de144583bff63461929ba240dc8d68475ad5
Josiann Zhou <zhou.josi@gmail.com>
2021-01-13 19:51:27 -0500

    made Groups page components
    without icons and breakpoints

--- frontend/src/AuthorizedPage/GroupsComponent/GroupsComponent.js
+++ frontend/src/AuthorizedPage/GroupsComponent/GroupsComponent.js
@@ -1,32 +1,48 @@
 import './GroupsComponent.css';
 
+//can't get three-dot or search icon to work
+//also don't get how to implement breakpoints
+
 //Component to display groups on Groups page
 const GroupsComponent = (props) => {
     return (
+        // Flexbox for whole component to make sure transition from mobile to desktop is smooth
         <div className="GroupsComponent d-flex flex-column align-left">
-            <h1 className="title item"><strong>Groups</strong></h1>
+            
+            {/* Flexbox for Existing Groups */}
             <div className="YourGroupsBox d-flex flex-column align-items-left">
+                <h1 className="title"><strong>Groups</strong></h1>
                 { /*search bar */}
-                <div className="search-bar item">
+                <div className="search-bar">
                     <i class="glyphicon glyphicon-search"></i>
                     <input type="text" class="input-search" placeholder="search"/>
                 </div>
+
                 { /*list of groups */}
-                <ul className="group-list item">
-                    <li><a>Group 1</a></li>
-                    <li><a>Group 2</a></li>
-                    <li><a>Group 3</a></li>
-                </ul>   
+                <div className="group-list">
+                    <div className="group-item d-flex">
+                        <div><a>Group 1</a></div>
+                        <div class="ml-auto"><a><img className="three-dots" src="/three-dots.svg"/></a></div>
+                    </div>
+                    <div className="group-item d-flex">
+                        <div><a>Group 1</a></div>
+                        <div class="ml-auto"><a><img className="three-dots" src="/three-dots.svg"/></a></div>
+                    </div>
+                    <div className="group-item d-flex">
+                        <div><a>Group 1</a></div>
+                        <div class="ml-auto"><a><img className="three-dots" src="/three-dots.svg"/></a></div>
+                    </div>
+                </div>   
             </div>
+
+            {/* Flexbox for creating or joining new group */}
             <div className="JoinOrCreateBox d-flex flex-column align-items-left">
-                <a>
-                    <i class="glyphicon glyphicon-plus"></i>
-                    <h6>Create a new group</h6>
-                </a>
-                <a>
-                    <i class="glyphicon glyphicon-plus"></i>
-                    <h6>Join a group</h6>
-                </a>
+                <div className="CreateNewGroup">
+                    <a>+ Create a new group</a>
+                </div>
+                <div className="JoinGroup">
+                    <a>+ Join a group</a>
+                </div>
             </div>
         </div>
     );
