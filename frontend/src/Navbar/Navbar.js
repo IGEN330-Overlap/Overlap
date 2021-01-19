ee5b9495b69e10266f794c863f241da03f08d546
Danica <danicadickinson@gmail.com>
2021-01-19 09:55:45 -0800

    Navbar update
    My groups, overlap logo, about us

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,13 +1,18 @@
 import './Navbar.css';
+import logo from './overlap large.svg'
 
 const Navbar = (props) => {
     return (
-        <nav class="navbar fixed-top navbar-light bg-light">
-            <span class="navbar-text">
-                Overlap
-            </span>
+        <nav class="navbar navbar-expand-lg fixed-top navbar-text">
+            <div class="container-fluid navbar-custom">
+                <a>
+                    <img class="change-my-color" width="100" height="100" src= {logo} alt="logo"/> 
+                </a>
+                <a>My Groups</a>
+                <a>About Us</a>
+            </div>                       
         </nav>
-    );
+    );  
 }
 
 export default Navbar;
