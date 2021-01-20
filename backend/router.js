7cfb736157259cba4fcbb222846a9821d9c6fdaa
remy <rzhang240@gmail.com>
2021-01-19 21:47:06 -0800

    Created a route for createGroup

--- backend/router.js
+++ backend/router.js
@@ -5,4 +5,7 @@ const Controller = require("./Controller.js");
 //define get request for retreiving user information
 router.get("/token/:token/user", Controller.getUser);
 
+router.post("/group/create", Controller.createGroup);
+
+
 module.exports = router;
