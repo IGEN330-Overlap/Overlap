2aa78ac51345eeb99043245017d2a76e2372d747
Josiann Zhou <zhou.josi@gmail.com>
2021-01-21 19:11:48 -0500

    adding modals and dropdowns

--- frontend/src/AuthorizedPage/CreateOrJoin/CreateOrJoin.js
+++ frontend/src/AuthorizedPage/CreateOrJoin/CreateOrJoin.js
@@ -1,16 +1,46 @@
 import './CreateOrJoin.css';
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+import continueArrow from './ContinueArrow.svg';
 
 //Component to display Create a new group or Join a group component
 const CreateOrJoin = (props) => {
+    
+    const [openCreate, setCreateOpen] = React.useState(false);
+    const [openJoin, setJoinOpen] = React.useState(false);
+    
     return (
         //Flexbox for creating or joining new group
         <div className="CreateOrJoinBox d-flex flex-column align-left">
-            <div className="CreateNewGroup">
-                <a>+   Create a new group</a>
-            </div>
-            <div className="JoinGroup">
-                <a>+   Join a group</a>
-            </div>
+            <a
+                onClick={() => setCreateOpen(!openCreate)}
+                aria-controls="example-collapse-text"
+                aria-expanded={openCreate}
+            >
+            <h5 className="openCollapse">+   Create a new group</h5>
+            </a>
+            <Collapse in={openCreate}>
+                <div id="example-collapse-text">
+                    <input type="text" class="input-code" placeholder="Enter Group Name" size="15"/>
+                    <a><img src={continueArrow}/></a>
+                </div>
+            </Collapse>
+
+            <a
+                onClick={() => setJoinOpen(!openJoin)}
+                aria-controls="example-collapse-text"
+                aria-expanded={openJoin}
+            >
+            <h5 className="openCollapse">+   Join a group</h5>
+            </a>
+            <Collapse in={openJoin}>
+                <div id="example-collapse-text">
+                    <input type="text" class="input-code" placeholder="Enter Group Code" size="15"/>
+                    <a><img src={continueArrow}/></a>
+                </div>
+            </Collapse>
+
         </div>
     );
 }
--- frontend/src/AuthorizedPage/GroupsComponent/GroupsComponent.js
+++ frontend/src/AuthorizedPage/GroupsComponent/GroupsComponent.js
@@ -1,10 +1,49 @@
-import CreateOrJoin from '../CreateOrJoin/CreateOrJoin';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import Modal from 'react-bootstrap/Modal';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 import './GroupsComponent.css';
 import dots from './three-dots.svg';
 import line from './Line.svg';
+import React from "react";
+
 
 //Component to display groups on Groups page
 const GroupsComponent = (props) => {
+    
+    //functions for opening and closing "Show Group Code" Modal
+    const [CodeisOpen, setCodeIsOpen] = React.useState(false);
+    const showCodeModal = () => {
+        setCodeIsOpen(true);
+    };
+    const hideCodeModal = () => {
+        setCodeIsOpen(false);
+    };
+
+    //functions for opening and closing "Leave Group" Modal
+    const [LeaveisOpen, setLeaveIsOpen] = React.useState(false);
+    const showLeaveModal = () => {
+        setLeaveIsOpen(true);
+    };
+    const hideLeaveModal = () => {
+        setLeaveIsOpen(false);
+    };
+
+    // //Display Dropdown Button
+    // const groupDropdown = () => (
+    //     <Dropdown as={ButtonGroup}>
+    //         <Button variant="success">Group 1</Button>
+    //         <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+    //         <Dropdown.Menu>
+    //             <Dropdown.Item href="#/action-1"><a onClick={showCodeModal}>Show Group Code</a></Dropdown.Item>
+    //             <Dropdown.Item href="#/action-2"><a onClick={showLeaveModal}>Leave Group</a></Dropdown.Item>
+    //         </Dropdown.Menu>
+    //     </Dropdown>
+    // );
+
+
     return (
         // Flexbox for existing groups
         <div className="YourGroupsBox d-flex flex-column align-left">
@@ -17,22 +56,53 @@ const GroupsComponent = (props) => {
             </div>
 
             { /*list of groups */}
-                <div className="group-list">
-                    <div className="group-item d-flex">
-                        <div><a>Group 1</a></div>
-                        <div class="ml-auto"><a><img src={dots} className="three-dots" alt="three dot menu"/></a></div>
-                    </div>
-                    <div className="group-item d-flex">
-                        <div><a>Group 1</a></div>
-                        <div class="ml-auto"><a><img src={dots} className="three-dots" alt="three dot menu"/></a></div>
-                    </div>
-                    <div className="group-item d-flex">
-                        <div><a>Group 1</a></div>
-                        <div class="ml-auto"><a><img src={dots} className="three-dots" alt="three dot menu"/></a></div>
-                    </div>
-                </div>   
-        </div>    
+            <div className="group-list">
+
+                {/* Group as a dropdown menu button */}
+                <div className="group-item d-flex">
+                    <Dropdown as={ButtonGroup}>
+                        <Button variant="dark" className="groupButton" text-left>Group 1</Button>
+                        <Dropdown.Toggle split variant="dark" className="groupToggle" id="dropdown-split-basic" />
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1"><a onClick={showCodeModal}>Show Group Code</a></Dropdown.Item>
+                            <Dropdown.Divider></Dropdown.Divider>
+                            <Dropdown.Item href="#/action-2"><a onClick={showLeaveModal}>Leave Group</a></Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+
+                {/* Group as a dropdown menu button
+                <div className="group-item d-flex">
+                    <groupDropdown/>
+                </div>  */}
+            </div>
+    
+            {/* "Show Group Code" Modal Content */}
+            <>
+                <Modal show={CodeisOpen} onHide={hideCodeModal} centered>
+                    <Modal.Body className="in-modal modal-body">
+                        <h5 className="modal-text"><strong>The Group Code for "Group Name" is</strong></h5>
+                        <h4 className="modal-text pad-below"><strong>"Group Code"</strong></h4>
+                        <button onClick={hideCodeModal} className="btn-in-modal continue-button" centered><strong>Continue</strong></button>
+                    </Modal.Body>
+                 </Modal>
+            </>
+
+            {/* "Leave Group" Modal Content */}
+            <>
+                <Modal show={LeaveisOpen} onHide={hideLeaveModal} centered>
+                    <Modal.Body className="in-modal">
+                        <h5 className="modal-text"><strong>Are you sure you want to leave?</strong></h5>
+                        <h4 className="modal-text pad-below"><strong>"Group Name"</strong></h4>
+                        <p><button className="btn-in-modal leave-buttons">Yes, I'm sure</button></p>
+                        <p><button onClick={hideLeaveModal} className="btn-in-modal leave-buttons">Nope, take me back</button></p>
+                    </Modal.Body>
+                </Modal>
+            </>
+
+        </div>                
     );
+
 }
 
 export default GroupsComponent;
