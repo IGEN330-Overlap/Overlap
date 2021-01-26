1bddd828addc8f5f5971cf3a9983bce6a33863cd
Danica <danicadickinson@gmail.com>
2021-01-25 20:15:23 -0800

    Right colour, overlap logo spacing fixed

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,13 +1,27 @@
 import './Navbar.css';
-import logo from './overlap large.svg'
+import logo from './overlap_image.jpg'
 
 const Navbar = (props) => {
     return (
-        <nav class="navbar navbar-expand-lg fixed-top navbar-text">
+        <nav class="navbar navbar-expand-lg fixed-top">
             <div class="container-fluid navbar-custom">
-                <img width="100" height="100" src= {logo} alt="logo"/> 
-                <a>My Groups</a>
-                <a>About Us</a>
+                 <img width="65" height="25" src= {logo} alt="logo"/>      
+            <div class= "collapse navbar-collapse" id= "navbarNavAltMarkup">
+                <ul class="navbar-nav me-auto mb-lg-0">
+                    <li class="nav-item dropdown">  
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown">
+                            My Groups
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                </ul>
+            </div>
             </div>                       
         </nav>
     );  
