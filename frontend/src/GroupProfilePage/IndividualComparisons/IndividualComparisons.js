6671129337029ff686d7edfa539d215a5c8aed58
Beatrice Tam <beatrice.6293@gmail.com>
2021-01-31 00:31:47 -0500

    added top tracks, adjusted top artist sizing

--- frontend/src/GroupProfilePage/IndividualComparisons/IndividualComparisons.js
+++ frontend/src/GroupProfilePage/IndividualComparisons/IndividualComparisons.js
@@ -2,9 +2,14 @@ import './IndividualComparisons.css';
 import icon1 from './conan.JPG'
 import icon2 from './valley.JPG';
 import icon3 from './daniel.JPG';
+import icon4 from './drivers.JPG';
 
 const myTopArtists = ['conan gray','valley','daniel caesar']
 const myTopArtistsIcons = [icon1,icon2,icon3]
+
+const myTopTracks = ['Heather','drivers license']
+const myTopTracksArtist = ['Conan Gray','Olivia Rodrigo']
+const myTopTracksIcon = [icon1,icon4]
 
 export const MyInsights = (props) => {
     return(
@@ -19,7 +24,25 @@ export const MyInsights = (props) => {
                         <div className="artist-container">
                             <img className="artist-icon" src={myTopArtistsIcons[i]} alt={artist}></img>
                             <div className="artist-name">
-                                    <strong>{artist.toUpperCase()}</strong>
+                                    <strong>{[i+1]} / {artist.toUpperCase()}</strong>
+                            </div>
+                        </div>
+                    ))
+                    }
+                </div>
+            </div>
+            <div className="top-tracks">
+                <h2><strong>Top Tracks</strong></h2>
+                <div className="track-display">
+                    {myTopTracks.map((track,i) => (
+                        <div className="track-container">
+                            <h3><strong>{[i+1]}</strong></h3>
+                            <img className="track-icon" src={myTopTracksIcon[i]} alt={track}/>
+                            <div className="track-info">    
+                                <strong>{track.toUpperCase()}</strong>
+                                <div className="track-artist">
+                                    <strong>{myTopTracksArtist[i]}</strong>
+                                </div>
                             </div>
                         </div>
                     ))
