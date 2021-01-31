1556ae25f034dd9d4ed97cc41c85973dcd45886c
Beatrice Tam <beatrice.6293@gmail.com>
2021-01-31 02:05:04 -0500

    comparisons component in individual comparison section

--- frontend/src/GroupProfilePage/IndividualComparisons/IndividualComparisons.js
+++ frontend/src/GroupProfilePage/IndividualComparisons/IndividualComparisons.js
@@ -11,6 +11,8 @@ const myTopTracks = ['Heather','drivers license']
 const myTopTracksArtist = ['Conan Gray','Olivia Rodrigo']
 const myTopTracksIcon = [icon1,icon4]
 
+const compare = ''
+
 export const MyInsights = (props) => {
     return(
         <div className="my-insights">
@@ -56,7 +58,41 @@ export const MyInsights = (props) => {
 export const Comparisons = (props) => {
     return(
         <div className="comparisons">
+            <h1><strong>Comparing You and {compare}</strong></h1>
+            <div className="under-bar"></div>
 
+            <div className="top-artists">
+                <h2><strong>Top Artists</strong></h2>
+                <div className="artist-display">
+                    {myTopArtists.map((artist,i) => (
+                        <div className="artist-container">
+                            <img className="artist-icon" src={myTopArtistsIcons[i]} alt={artist}></img>
+                            <div className="artist-name">
+                                    <strong>{[i+1]} / {artist.toUpperCase()}</strong>
+                            </div>
+                        </div>
+                    ))
+                    }
+                </div>
+            </div>
+            <div className="top-tracks">
+                <h2><strong>Top Tracks</strong></h2>
+                <div className="track-display">
+                    {myTopTracks.map((track,i) => (
+                        <div className="track-container">
+                            <h3><strong>{[i+1]}</strong></h3>
+                            <img className="track-icon" src={myTopTracksIcon[i]} alt={track}/>
+                            <div className="track-info">    
+                                <strong>{track.toUpperCase()}</strong>
+                                <div className="track-artist">
+                                    <strong>{myTopTracksArtist[i]}</strong>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
