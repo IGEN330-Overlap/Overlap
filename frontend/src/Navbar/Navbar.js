27c16cf0579feb04a5275d3e9ee36637a3253d7f
Danica <danicadickinson@gmail.com>
2021-01-30 16:40:39 -0800

    Auto stash before merge of "danica/about-us" and "master"

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,24 +1,19 @@
 import './Navbar.css';
-import logo from './overlap_image.jpg'
+import logo from './overlap_image.jpg';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
     return (
         <nav class="navbar navbar-text navbar-expand-lg fixed-top">
-            <div class="container-fluid navbar-custom">
-                 <img width="65" height="25" src= {logo} alt="logo"/>      
+            <div class="container-fluid">
+                 <img width="75" height="30" src= {logo} alt="logo"/>      
             <div class= "collapse navbar-collapse" id= "navbarNavAltMarkup">
-                <ul class="navbar-nav me-auto mb-lg-0">
-                    <li class="nav-item dropdown">  
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown">
-                            My Groups
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                        </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">  
+                        <Link to ="GroupProfilePage/GroupProfilePage">My Groups</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./AboutUs">About Us</a>
+                        <Link to ="AboutUs/AboutUs">About Us</Link> 
                     </li>
                 </ul>
             </div>
