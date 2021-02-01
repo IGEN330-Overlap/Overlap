51bceed3760430d57cf587b14f42ca03a4098905
Brendan Lai <laibrendna8@gmail.com>
2021-02-01 01:58:02 -0500

    response formats for getTopArtists
    more uncovering in testing. Will be used to help determine the format of requests and how to structure data for the algorithm

--- backend/Algorithm/algoTest.js
+++ backend/Algorithm/algoTest.js
@@ -24,19 +24,34 @@ exports.notFunTest = async (req, res) => {
             spotifyApi.setAccessToken(data.body.access_token);
 
             // Get a user's top Playlists
-            spotifyApi.getMyTopTracks().then(
+            // spotifyApi.getMyTopTracks().then(
+            //     (data) => {
+            //         for (x of data.body.items) {
+            //             console.log("ID is" + x['id'])
+            //             console.log("Name is " + x['name'])
+            //             // console.log("Artist is " + x['artists'])
+            //             // Figure out how to do artist objects
+            //             console.log("Popularity is " + x['popularity'] + "\n");
+            //         }
+            //     },
+            //     //SpotifyAPI return error
+            //     (err) => {
+            //         res.status(400).json(err);
+            //     }
+            // )
+            spotifyApi.getMyTopArtists().then(
                 (data) => {
-                    for (x of data.body.items) {
-                        console.log("ID is" + x['id'])
-                        console.log("Name is " + x['name'])
-                        console.log("Artist is " + x['artists'])
-                        console.log("Popularity is " + x['popularity'] + "\n");
+                    for (x of data.body.items){
+                        console.log("Name is: " + x['name'])
+                        console.log("Popularity score is: " + x['popularity'])
+                        console.log("Common genres are: " + x['genres'] + "\n")
                     }
                 },
                 //SpotifyAPI return error
                 (err) => {
                     res.status(400).json(err);
-                })
+                }
+            )
         },
         (err) => {
             res.json("Unable to refresh access token.");
