0750312f50a0bdbdbe3e7befb06d71cc2d40b07d
Danica <danicadickinson@gmail.com>
2021-02-04 13:32:52 -0800

    About us upgrade
    Made playlist folder, baby pics, about us

---
+++ frontend/src/AboutUs/BabyPics/BabyPics.js
@@ -0,0 +1,31 @@
+import './BabyPics.css';
+import Brendan from './Brendan.png';
+import Kevin from './Kevin.jpg';
+import Beatrice from './Beatrice.jpg';
+import Josi from './Josi.jpg';
+import Danica from './Danica.jpg';
+
+const Babies = (props) => {
+    return(
+        <div className= "baby-members">
+            <div className= 'container-baby'>
+                <img className='icon' src={Danica} alt= 'Danica'></img></div>
+                    <div className="name">
+                                <strong>Danica</strong>
+                        </div>
+            <div className= 'container-baby'>
+                <img className='icon' src={Kevin} alt= 'Kevin'></img></div>
+            
+            <div className= 'container-baby'>
+                <img className='icon' src={Josi} alt= 'Josi'></img></div>
+            
+            <div className= 'container-baby'>
+                <img className='icon' src={Brendan} alt= 'Brendan'></img></div>
+
+            <div className= 'container-baby'>    
+                <img className='icon' src={Beatrice} alt= 'Beatrice'></img></div>
+        </div>
+    )
+}
+
+export default Babies;
---
+++ frontend/src/AboutUs/TextBlurb/TextBlurb.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TextBlurb.css';
+
+const Blurb = (props) => {
+    return(
+    <div className="about-name-container">
+        <h1 className="text"><strong>About us</strong></h1>
+        <div className="under-bar"></div>
+        <p className='sub-text'>Hey! Glad you made it. Overlap is a class project built by a few students at UBC.</p> 
+        <p className='sub-text'>If you have any question, comments, or concerns, please reach out!</p>
+    </div>
+    )
+}
+
+export default Blurb;
