fd053308bb22301a908278ff039c296f1c0211d9
Kevin Peng <kevinpkf311@gmail.com>
2021-02-08 21:23:00 -0800

    Refactored user model, implemented top artist+track retrieval on login

--- backend/Models/user.model.js
+++ backend/Models/user.model.js
@@ -1,38 +1,98 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 // Building user schema and defining the fields
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     userID: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
     refreshToken: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
+      type: String,
     },
     imageURL: {
-        type: String
+      type: String,
     },
     email: {
-        type: String
+      type: String,
     },
-    groups: [
-        {
-            type: String
+
+    topTracks: [
+      {
+        songName: {
+          type: String,
+          required: true,
         },
+        songID: {
+          type: String,
+          required: true,
+        },
+        songPopularity: {
+          type: Number,
+          required: true,
+        },
+        imageURL: {
+          type: String,
+          required: true,
+        },
+        linkURL: {
+          type: String,
+          required: true,
+        },
+        artistName: {
+          type: String,
+          required: true,
+        },
+      },
     ],
-},
-    {
-        timestamps: true,
-    }
+
+    topArtists: [
+      {
+        artistName: {
+          type: String,
+          required: true,
+        },
+        artistID: {
+          type: String,
+          required: true,
+        },
+        followerCount: {
+          type: Number,
+          required: true,
+        },
+        artistPopularity: {
+          type: Number,
+          required: true,
+        },
+        imageURL: {
+          type: String,
+          required: true,
+        },
+        linkURL: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    groups: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
