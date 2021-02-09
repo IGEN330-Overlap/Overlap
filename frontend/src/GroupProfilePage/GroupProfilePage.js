983a38387ba81167ca432d29886c81ec75f2aaaf
Beatrice Tam <beatrice.6293@gmail.com>
2021-02-08 22:18:21 -0500

    small css fixes to comparison specification

--- frontend/src/GroupProfilePage/GroupProfilePage.js
+++ frontend/src/GroupProfilePage/GroupProfilePage.js
@@ -12,7 +12,6 @@ const GroupProfilePage = (props) => {
 
     let { path, url } = useRouteMatch();
 
-
     const [name, selectMember] = useState('');
     function toCompare(value){
         selectMember(value);
