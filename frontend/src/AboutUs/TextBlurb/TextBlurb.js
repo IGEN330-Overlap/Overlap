69a135325756a8c02f524cac02181e166e719617
Danica <danicadickinson@gmail.com>
2021-02-13 14:31:44 -0800

    Navbar collapse
    Doesn't do weird collapse thing anymore,

--- frontend/src/AboutUs/TextBlurb/TextBlurb.js
+++ frontend/src/AboutUs/TextBlurb/TextBlurb.js
@@ -4,7 +4,7 @@ import './TextBlurb.css';
 const Blurb = (props) => {
     return(
     <div className="about-name-container">
-        <h1 className="text"><strong>About us</strong></h1>
+        <h1 className="big-text"><strong>About us</strong></h1>
         <div className="under-bar"></div>
         <p className='sub-text'>Hey! Glad you made it. Overlap is a class project built by a few students at UBC.</p> 
         <p className='sub-text'>If you have any question, comments, or concerns, please reach out!</p>
