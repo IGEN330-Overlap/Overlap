b3cae201dc0a7fc3577c1431101c9499d87d9fc2
Kevin Peng <kevinpkf311@gmail.com>
2021-02-25 19:18:56 -0800

    Initial state implementation
    Implemented Redux state for global app states including user  object, refreshToken, etc
    
    Refactored existing state logic to use global redux state store. Implemented user login with our API using axios and useEffect hook upon refreshToken update.

---
+++ frontend/src/Redux/Actions.js
@@ -0,0 +1,19 @@
+//Set refresh token action
+export const updateRefreshToken = (newRefreshToken = "") => {
+  return {
+    type: "refreshToken/update",
+    payload: {
+      refreshToken: newRefreshToken,
+    },
+  };
+};
+
+//Set user object action
+export const updateUser = (newUser = null) => {
+  return {
+    type: "userObject/update",
+    payload: {
+      newUser: newUser,
+    },
+  };
+};
