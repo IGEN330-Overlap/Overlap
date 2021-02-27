89fc6833e810d6eac7d595d045ffac70a352213a
Danica <danicadickinson@gmail.com>
2021-02-27 13:28:12 -0800

    Navbar links fixed
    Logout button formatting

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -14,23 +14,25 @@ const Navbar1 = (props) => {
     return (
         <div className='navbar-stuff'>
         <Navbar bg="transparent" variant="dark" expand="sm">
-            <Navbar.Brand href="/authorized/"><img width="75" height="30" src= {logo} alt="logo"/></Navbar.Brand>
+            <Navbar.Brand as={Link} to = "/authorized/"><img width="75" height="30" src= {logo} alt="logo"/></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <NavDropdown title="My Groups" id="basic-nav-dropdown">
                         {groupnames.map((group,i) => (
-                        <div>
-                        <NavDropdown.Item> <Link to = "/authorized/GroupProfilePage">{group}</Link></NavDropdown.Item>
-                        </div>))}
+                            <div>
+                                <NavDropdown.Item as={Link} to = "/authorized/GroupProfilePage">{group}</NavDropdown.Item>
+                            </div>
+                            ))
+                        }
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="/authorized/">New Group</NavDropdown.Item>
-                        </NavDropdown> 
-                    <Nav.Link bg="white" href="/authorized/AboutUs/">About Us</Nav.Link>
-                    </Nav>
-                    <Nav className="justify-content-end">
-                        <Button variant='info' size='sm'>Logout</Button>
-                    </Nav>
+                        <NavDropdown.Item as={Link} to = "/authorized/">New Group</NavDropdown.Item>
+                    </NavDropdown> 
+                    <Nav.Link as={Link} to = "/authorized/AboutUs" bg="white">About Us</Nav.Link>
+                </Nav>
+                <Nav className="justify-content-end">
+                    <Button as={Link} to ="/" variant='secondary' size='sm'>Logout</Button>
+                </Nav>
             </Navbar.Collapse>
         </Navbar>
         </div>
