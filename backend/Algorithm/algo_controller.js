96a44508e7d49ab84c831f776042953acab798e6
Brendan Lai <laibrendna8@gmail.com>
2021-03-02 03:03:00 -0500

    added function for building spotify playlist
    uses a dumb algorithm that just takes the top5 songs of everyone and makes a playlist out of that

--- backend/Algorithm/algo_controller.js
+++ backend/Algorithm/algo_controller.js
@@ -1,7 +1,9 @@
 // Don't bother using the song and artist class
 const SpotifyWebApi = require("spotify-web-api-node");
+// const { db } = require("../Models/user.model");
 require("dotenv").config();
 
+const overlapLogo = process.env.OVERLAP_LOGO;
 const client_id = process.env.CLIENT_ID; // Your client id
 const client_secret = process.env.CLIENT_SECRET; // Your secret
 const redirect_uri = process.env.BACKEND_URL + "callback"; // Your redirect uri
@@ -11,6 +13,9 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret: client_secret,
   redirectUri: redirect_uri,
 });
+
+let User = require("../Models/user.model");
+// let Group = require("../Models/group.model");
 
 exports.getMyTopTracks = async (req, res) => {
   //set refresh token
@@ -83,3 +88,86 @@ exports.getMyTopTracks = async (req, res) => {
     }
   );
 };
+
+exports.buildSpotifyPlaylist = async (req, res) => {
+  
+  topTracks = []; // Array to store the total data from mongoDB
+
+  // Map the user ids sent to get the mongoDB user information
+  var user_ids = req.body.userIDs.map((id) => {return id});
+  await User.find({userID: {$in: user_ids}})
+    .then((data) => {
+      // topTracks = data.topTracks.map((trackList) => {return trackList})
+      // console.log(topTracks)
+      for (x of data){
+        topTracks.push(x.topTracks);
+      }
+      // res.json(topTracks)
+    })
+    .catch((err) =>{
+      res.json(err)
+    })
+  
+    playlist = [];
+
+    // DO THE ALGORITHM, TO IMPLEMENT
+    for(var i = 0; i < topTracks.length; i++){
+      for(var j = 0; j < 5; j++){
+        playlist.push("spotify:track:" + topTracks[i][j].trackID);
+      }
+    }
+
+    spotifyApi.setRefreshToken(req.body.refreshToken);
+  // Connect to spotify API using the owners refresh access token
+  await spotifyApi.refreshAccessToken()
+    .then( async (data) => {
+      spotifyApi.setAccessToken(data.body.access_token);
+      
+      var playlistID; // playlistID to be used in adding to the playlist
+      await spotifyApi
+        .createPlaylist("Overlap YOLO", {'description': "Gang", 'public': true})
+        .then((data) => {
+          console.log("Playlist Created") 
+          playlistID = data.body.id;
+
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      
+      await spotifyApi
+        .addTracksToPlaylist(playlistID, playlist)
+        .then((data) =>{
+          console.log('Added tracks to playlist!', data.statusCode);
+        })
+        .catch((err) => {
+          console.log('Something went wrong!', err);
+        });
+      // await spotifyApi
+      //   .uploadCustomPlaylistCoverImage(playlistID, overlapLogo)
+      //   .then((data) => {
+      //     console.log("uploaded playlist image", data)
+      //   })
+      //   .catch((err) => {
+      //     console.log(err)
+      //   })
+    
+    res.json({
+      message: "Successfully created playlist",
+      return: {
+        playlistID: playlistID,
+        // playlistOwner: req.body.userID,
+        playlistTracks: playlist,
+      }
+    });
+
+  })
+}
+
+/* 
+THINK ABOUT LOGIN and the scopes
+Algorithm Development
+Integrating a base64 for the playlist Logo
+TODO build playlist model
+outline integrating the playlist model and the necessary endpoints as well as adding it into this one
+*/
