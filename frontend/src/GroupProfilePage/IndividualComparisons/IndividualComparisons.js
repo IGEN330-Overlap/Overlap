9496a9a733520839b5f67386498957de2d601490
Beatrice Tam <beatrice.6293@gmail.com>
2021-03-07 12:10:52 -0500

    adjusted individual comparisons to account for ranking

--- frontend/src/GroupProfilePage/IndividualComparisons/IndividualComparisons.js
+++ frontend/src/GroupProfilePage/IndividualComparisons/IndividualComparisons.js
@@ -2,39 +2,19 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import './IndividualComparisons.css';
 
-// import icon1 from './conan.JPG'
-// import icon6 from './heroe.JPG';
-// import icon7 from './running.JPG';
-// import icon8 from './elephant.JPG';
-// import icon10 from './joji.JPG';
-// import icon11 from './ts.JPG';
-
-// const compareArtists = ['Joji','conan gray','taylor swift']
-// const compareArtistsIcons = [icon10,icon1,icon11]
-
-// const compareTracks = ['Heroes - 2017 Remaster','We Come Running','Come A Little Closer']
-// const compareTracksArtist = ['David Bowie','Youngblood Hawke','Cage The Elephant']
-// const compareTracksIcon = [icon6,icon7,icon8]
-
 export const MyInsights = (props) => {
        
     const getTopArtists = useSelector((state) => state.userObject.topArtists).slice(0,3);
     const myTopArtists = []
-    const myTopArtistsIcons = []
     getTopArtists.map((artist,i) => {
-        myTopArtists[i]=artist.artistName
-        myTopArtistsIcons[i]=artist.imageURL
+        myTopArtists[i]=({artist: artist.artistName, icon: artist.imageURL})
         return myTopArtists
     })
 
     const getTopTracks = useSelector((state) => state.userObject.topTracks).slice(0,3);
     const myTopTracks = []
-    const myTopTracksArtist = []
-    const myTopTracksIcon = []
     getTopTracks.map((track,i) => {
-        myTopTracks[i]=track.trackName
-        myTopTracksArtist[i]=track.artistName
-        myTopTracksIcon[i]=track.imageURL
+        myTopTracks[i] = ({track: track.trackName, artist: track.artistName, icon: track.imageURL})
         return myTopTracks
     })
     
@@ -50,10 +30,10 @@ export const MyInsights = (props) => {
                 <div className="artist-display">
                     {myTopArtists.map((artist,i) => (
                         <div className="artist-container">
-                            <img className="artist-icon" src={myTopArtistsIcons[i]} alt={artist}></img>
+                            <img className="artist-icon" src={artist.icon} alt={artist.artist}></img>
                             <div className="artist-name">
                                 <h5><strong>{[i+1]} / </strong></h5>
-                                <strong className="artist">{artist.toUpperCase()}</strong>
+                                <strong className="artist">{artist.artist.toUpperCase()}</strong>
                             </div>
                         </div>
                     ))
@@ -66,13 +46,13 @@ export const MyInsights = (props) => {
                     {myTopTracks.map((track,i) => (
                         <div className="track-container">
                             <h3><strong>{[i+1]}</strong></h3>
-                            <img className="track-icon" src={myTopTracksIcon[i]} alt={track}/>
+                            <img className="track-icon" src={track.icon} alt={track.track}/>
                             <div className="track-info">    
                                 <div className="track-title">    
-                                    <strong>{track.toUpperCase()}</strong>
+                                    <strong>{track.track.toUpperCase()}</strong>
                                 </div>
                                 <div className="track-artist">
-                                    <strong>{myTopTracksArtist[i]}</strong>
+                                    <strong>{track.artist}</strong>
                                 </div>
                             </div>
                         </div>
@@ -89,21 +69,15 @@ export const Comparisons = ({member_id, toCompare}) => {
     // user top artists and tracks
     const getTopArtists = useSelector((state) => state.userObject.topArtists)
     const myTopArtists = []
-    const myTopArtistsIcons = []
     getTopArtists.map((artist,i) => {
-        myTopArtists[i]=artist.artistName
-        myTopArtistsIcons[i]=artist.imageURL
+        myTopArtists[i]=({artist: artist.artistName, icon: artist.imageURL})
         return myTopArtists
     })
 
     const getTopTracks = useSelector((state) => state.userObject.topTracks)
     const myTopTracks = []
-    const myTopTracksArtist = []
-    const myTopTracksIcon = []
     getTopTracks.map((track,i) => {
-        myTopTracks[i]=track.trackName
-        myTopTracksArtist[i]=track.artistName
-        myTopTracksIcon[i]=track.imageURL
+        myTopTracks[i]=({track:track.trackName, artist: track.artistName, icon: track.imageURL})
         return myTopTracks
     })
 
@@ -120,56 +94,50 @@ export const Comparisons = ({member_id, toCompare}) => {
     })
 
     var userCompareArtists = []
-    var userCompareArtistsIcons = []
     var userCompareTracks = []
-    var userCompareTracksArtist = []
-    var userCompareTracksIcon = []
 
     compare_info.topArtists.map((artist,i) => {
-        userCompareArtists[i] = artist.artistName
-        userCompareArtistsIcons[i] = artist.imageURL
+        userCompareArtists[i] = ({artist: artist.artistName, icon: artist.imageURL})
         return userCompareArtists
     })
     
     compare_info.topTracks.map((track,i) => {
-        userCompareTracks[i] = track.trackName
-        userCompareTracksArtist[i] = track.artistName
-        userCompareTracksIcon[i] = track.imageURL
+        userCompareTracks[i] = ({track: track.trackName, artist: track.artistName, icon: track.imageURL})
         return userCompareTracks
     })
 
     // find up to three common artists
     const compareArtists = []
-    const compareArtistsIcons = []
     const compareTracks = []
-    const compareTracksArtist = []
-    const compareTracksIcon = []
 
     myTopArtists.map((myArtist, i) => {
         userCompareArtists.map((compArtist, j) => {
-            if(myArtist === compArtist){
-                compareArtists.push({artist: myArtist, rank: i+j})
-                compareArtistsIcons.push(myTopArtistsIcons[i])
+            if(myArtist.artist === compArtist.artist){
+                compareArtists.push({rank: i+j, artist: myArtist.artist, icon: myArtist.icon})
             }
             return null
         })
         return null
     })
-    console.log(compareArtists)
+
+    compareArtists.sort(function(a,b){
+        return a.rank - b.rank
+    })
 
     myTopTracks.map((myTrack, i) => {
         userCompareTracks.map((compTrack, j) => {
-            if(myTrack === compTrack){
-                compareTracks.push({track: myTrack, rank: i+j})
-                compareTracksArtist.push(myTopTracksArtist[i])
-                compareTracksIcon.push(myTopTracksIcon[i])
+            if(myTrack.track === compTrack.track){
+                compareTracks.push({rank: i+j, track: myTrack.track, artist: myTrack.artist, icon: myTrack.icon})
             }
             return null
         })
         return null
     })
-    console.log(compareTracks)
-
+    
+    compareTracks.sort(function(a,b){
+        return a.rank - b.rank
+    })
+    
     return(
         <div className="comparisons">
             <div className="return-to-insights">
@@ -187,7 +155,7 @@ export const Comparisons = ({member_id, toCompare}) => {
                 <div className="artist-display">
                     {compareArtists.slice(0,3).map((artist,i) => (
                         <div className="artist-container">
-                            <img className="artist-icon" src={compareArtistsIcons[i]} alt={artist.artist}></img>
+                            <img className="artist-icon" src={artist.icon} alt={artist.artist}></img>
                             <div className="artist-name">
                                 <h5><strong>{[i+1]} / </strong></h5>
                                 <strong className="artist">{artist.artist.toUpperCase()}</strong>
@@ -203,13 +171,13 @@ export const Comparisons = ({member_id, toCompare}) => {
                     {compareTracks.slice(0,3).map((track,i) => (
                         <div className="track-container">
                             <h3><strong>{[i+1]}</strong></h3>
-                            <img className="track-icon" src={compareTracksIcon[i]} alt={track.track}/>
+                            <img className="track-icon" src={track.icon} alt={track.track}/>
                             <div className="track-info">
                                 <div className="track-title">    
                                     <strong>{track.track.toUpperCase()}</strong>
                                 </div>
                                 <div className="track-artist">
-                                    <strong>{compareTracksArtist[i]}</strong>
+                                    <strong>{track.artist}</strong>
                                 </div>
                             </div>
                         </div>
