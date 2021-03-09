01c43a631b8074cce34a0fa8b9162fde62525547
Brendan Lai <laibrendan8@gmail.com>
2021-03-08 21:07:00 -0500

    algo, count top tracks implemented
    algorithm now adds commonoly occuring top tracks. Some simple math is used to outline what is listed as acceptable. currently only adds those top tracks (20 of them)

--- backend/Controllers/algorithmController.js
+++ backend/Controllers/algorithmController.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 let User = require("../Models/user.model");
 let Group = require("../Models/group.model");
+let Scripts = require("../scripts.js")
 
 const client_id = process.env.CLIENT_ID; // Your client id
 const client_secret = process.env.CLIENT_SECRET; // Your secret
@@ -13,9 +14,9 @@ const redirect_uri = backend_url + "callback"; // Your redirect uri
 
 // instantiate spotifyApi object
 var spotifyApi = new SpotifyWebApi({
-  clientId: client_id,
-  clientSecret: client_secret,
-  redirectUri: redirect_uri,
+    clientId: client_id,
+    clientSecret: client_secret,
+    redirectUri: redirect_uri,
 });
 
 
@@ -25,79 +26,139 @@ var spotifyApi = new SpotifyWebApi({
  * @param {userIDs: [Strings], groupCode: String} req 
  * @param {*} res 
  */
- exports.generateGroupsTopPlaylist = async (req, res) => {
+exports.generateGroupsTopPlaylist = async (req, res) => {
 
-    topTracks = []; // Array to store the total data from mongoDB
+    usersTopTracks = []; // stores each users top tracks as a single item
 
-    let userIDs = req.body.userIDs.map((id) => {return id});
+    let userIDs = req.body.userIDs.map((id) => { return id });
+    let numUsers = userIDs.length;
 
+    // Collect user top track data into top tracks array
     try {
-        let data = await User.find({userID: {$in: userIDs}});
+        let data = await User.find({ userID: { $in: userIDs } });
 
         // Add each persons dataset to our master array
-        for(x of data){ topTracks.push(x.topTracks); }
+        for (x of data) { usersTopTracks.push(x.topTracks); }
 
     } catch (err) {
-        res.json({message: "error on finding users", error: err});
+        res.json({ message: "error on finding users", error: err });
     }
 
-    let playlistTracks = [];
+    let playlistTracksAndInfo = []; // array to store the tracks of our playlist
 
-    /* Let's algorithm */
-    let masterSet = [];
-    let masterIDs = []
+    let masterSet = []; //stores all top tracks as an item
+    let masterIDs = []; //stores all track IDs as an item
 
     // put all the songs into one array, the master set
-    for (x of topTracks){
-        for (data of x){
+    for (x of usersTopTracks) {
+        for (data of x) {
             masterSet.push(data);
+            masterIDs.push(data.trackID)
         }
     }
 
-    for (x of masterSet) {
-        masterIDs.push(x.trackID);
-    }
+    /* Let's algorithm: This is making a 30 song playlist */
 
     // Count occurences of songs in master set
     let counts = masterIDs.reduce((a, c) => {
-        a[c] = (a[c] || 0) +1;
+        a[c] = (a[c] || 0) + 1;
         return a;
     }, {});
 
-    // find max count
+    // find max count of common occurences in the masterID set
     let maxCount = Math.max(...Object.values(counts));
-    // uncover most commonly occuring ids
-    let mostFrequentTracks = Object.keys(counts).filter(k => counts[k] >= maxCount - 1); 
+    // uncover most commonly occuring ids and store to most Frequent tracks
 
-    var j = 0;
+    let playlistTracks = []; // Array to store songs to be added to playlist
+    var numSongs = 0; // used to keep track of how many songs to add in each step
 
-    for (var i = 0; i < masterSet.length; i++) {
-        if (mostFrequentTracks[j] == masterSet[i].trackID){
-            playlistTracks.push({
-                trackName: masterSet[i].trackName,
-                trackID: masterSet[i].trackID,
-                imageUrl: masterSet[i].imageURL,
-                linkURL: masterSet[i].linkURL,
-                artistName: masterSet[i].artistName
-            })
+    /*
+    NOTE: what happens if one person is in the group and tries to make a playlist?????
+    algo off the top of my head would no likey, needs small adjustment at top of this block of ifs 
+    */
+
+    // If there are at least 2 users and max count occurs in at least half the users
+    if (numUsers <= 2 && maxCount == 2){
+        let mostFrequentTracks = Object.keys(counts).filter(k => counts[k] === maxCount);
+        // Each nested if block in this set of if/elif statements does the same thing
+        // add first 20 tracks if there are that many
+        // else add however many songs there are that occur maxCount times
+
+        if (mostFrequentTracks.length > 20){
+            playlistTracks = mostFrequentTracks.slice(0, 20);
+            numSongs = 20;
+        } else {
+            playlistTracks = mostFrequentTracks;
+            numSongs = mostFrequent
+            Tracks.length;
+        }
+    // If there are less than 7 users add if maxCount > (half the users rounded up)
+    } else if (numUsers <= 7 && maxCount >= numUsers - Math.floor(numUsers / 2) ){
+        let numOccurencesOfSongsToAdd = numUsers - Math.floor(numUsers / 2);
+        let mostFrequentTracks = Object.keys(counts).filter(k => counts[k] <= numOccurencesOfSongsToAdd);
+
+        if (mostFrequentTracks.length > 20){
+            playlistTracks = mostFrequentTracks.slice(0, 20);
+            numSongs = 20;
+        } else {
+            playlistTracks = mostFrequentTracks;
+            numSongs = mostFrequentTracks.length;
+        }
+    // If there are more than 7 users add if maxCount > (half the users rounded down)
+    } else if (numUsers > 7 && maxCount >= numUsers - Math.ceil(numUsers / 2)) {
+        let numOccurencesOfSongsToAdd = numUsers - Math.ceil(numUsers / 2);
+        let mostFrequentTracks = Object.keys(counts).filter(k => counts[k] <= numOccurencesOfSongsToAdd);
+
+        if (mostFrequentTracks.length > 20){
+            playlistTracks = mostFrequentTracks.slice(0, 20);
+            numSongs = 20;
+        } else {
+            playlistTracks = mostFrequentTracks;
+            numSongs = mostFrequentTracks.length;
+        }
+    }
+
+    // Using track sounds to add the rest of the songs
+
+    /* End of algorithm */
+
+    // Parses master set and adds the generate playlist tracks and the necessary info too
+    // This can def be optimized
+
+    var i, j = 0;
+    for (i = 0; playlistTracksAndInfo.length < playlistTracks.length; i++) {
+        // trackIDs match then add the corresponding data
+        if (playlistTracks[j] == masterSet[i].trackID) {
+            playlistTracksAndInfo.push(
+                {
+                    trackName: masterSet[i].trackName,
+                    trackID: masterSet[i].trackID,
+                    imageUrl: masterSet[i].imageURL,
+                    linkURL: masterSet[i].linkURL,
+                    artistName: masterSet[i].artistName
+                }
+            )
+            // iterate to next track and reset the master to retraverse from left
             j++;
+            i = 0;
         }
     }
 
     playlist = {
         playlistName: req.body.playlistName,
-        tracks: playlistTracks
+        tracks: playlistTracksAndInfo
     }
 
     try {
         let data = await Group.updateOne(
-                { groupCode: req.body.groupCode },
-                { $addToSet: { playlists: playlist } }
-            )
-        res.json({ 
+            { groupCode: req.body.groupCode },
+            { $addToSet: { playlists: playlist } }
+        )
+        res.json({
             message: "added playlist to the group",
-            playlist: playlist}
-            );
+            playlist: playlist
+        }
+        );
 
     } catch (err) {
         res.json({
@@ -115,18 +176,18 @@ var spotifyApi = new SpotifyWebApi({
  * @param {*} res 
  */
 exports.createSpotifyPlaylist = async (req, res) => {
-    // Connect to spotify API using the owners refresh access token
-    let playlistName;
-    let tracks = [];
 
-    // spotifyApi.setRefreshToken(req.body.refreshToken);
+    // Connect to spotify API using the owners refresh access token
+    spotifyApi.setRefreshToken(req.body.refreshToken);
+
+    // find the group and the corresponding playlistID (playlist object ID)
     try {
         let data = await Group.findOne(
-                { groupCode: req.body.groupCode },
-                { playlist: { $elemMatch: { _id: req.body.playlistID}}},
-            )
+            { groupCode: req.body.groupCode },
+            { playlist: { $elemMatch: { _id: req.body.playlistID } } },
+        )
         res.json(data) // SHIT DOESN'T WORK
-        
+
     } catch (err) {
         res.json(err)
     }
@@ -138,6 +199,6 @@ exports.createSpotifyPlaylist = async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-exports.getGroupPlaylist = async (req, res) =>{
+exports.getGroupPlaylist = async (req, res) => {
     console.log("EMPTY");
 }
