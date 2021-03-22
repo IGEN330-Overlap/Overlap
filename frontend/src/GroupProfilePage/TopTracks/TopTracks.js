cc858e0823dc1788afec24d0f27728d4eb2db873
Beatrice Tam <beatrice.6293@gmail.com>
2021-03-22 13:37:24 -0400

    group top tracks css

--- frontend/src/GroupProfilePage/TopTracks/TopTracks.js
+++ frontend/src/GroupProfilePage/TopTracks/TopTracks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TopTracks.css';
 
 import Carousel from "react-bootstrap/Carousel";
@@ -52,59 +52,65 @@ export const GroupTopTracks = ({groupUsers}) => {
         (v, i, a) => a.findIndex((t) => t.trackID === v.trackID) === i
     );
 
-  // Add 4 elements at a time to carousel array
-  let carouselTracks = [];
-  for (let i = 0; i < groupUniqueSongs.length; i += 9) {
-    let threeTracks = groupUniqueSongs.slice(i, i + 9);
+    // get screen width to determine how many items in the carousel
+    const [width, getWidth] = useState(window.innerWidth)
 
-    let carouselElement = threeTracks.map((track, i) => {
-      return (
-        <div className="track-container" key={i}>
-            <h3><strong>{[i+1]}</strong></h3>
-            <img className="track-icon" src={track.imageURL} alt={track.trackName}/>
-            <div className="track-info">
-                <div className="track-title">    
-                    <strong>{track.trackName.toUpperCase()}</strong>
-                </div>
-                <div className="track-artist">
-                    <strong>{track.artistName}</strong>
+    useEffect(() => {
+        const handleResize = () => {
+            getWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+    
+    var num_tracks
+    if (width >= 1200) {
+        num_tracks = 9
+    }
+    else if (width >= 800) {
+        num_tracks = 6
+    }
+    else if (width < 800) {
+        num_tracks = 3
+    }
+
+    // Add 4 elements at a time to carousel array
+    let carouselTracks = [];
+    for (let i = 0; (i < groupUniqueSongs.length) && (i < 36); i += num_tracks) {
+        let threeTracks = groupUniqueSongs.slice(i, i + num_tracks);
+
+        let carouselElement = threeTracks.map((track, j) => {
+        return (
+            <div className="track-container" key={j}>
+                <h3><strong>{[i+j+1]}</strong></h3>
+                <img className="track-icon" src={track.imageURL} alt={track.trackName}/>
+                <div className="track-info">
+                    <div className="track-title">    
+                        <strong>{track.trackName.toUpperCase()}</strong>
+                    </div>
+                    <div className="track-artist">
+                        <strong>{track.artistName}</strong>
+                    </div>
                 </div>
             </div>
-        </div>
-      );
-    });
-    carouselTracks.push(carouselElement);
-  }
+        );
+        });
+        carouselTracks.push(carouselElement);
+    }
 
     return(
         <div className="top-tracks-root">
             <div className="top-tracks-heading">
                 <h1 className="text"><strong>Top Tracks</strong></h1>
                 <div className="under-bar"></div>
-                    {/* {groupUniqueSongs.slice(0,18).map((track,i) => (
-                        <div className="track-container" key={i}>
-                            <h3><strong>{[i+1]}</strong></h3>
-                            <img className="track-icon" src={track.imageURL} alt={track.trackName}/>
-                            <div className="track-info">
-                                <div className="track-title">    
-                                    <strong>{track.trackName.toUpperCase()}</strong>
-                                </div>
-                                <div className="track-artist">
-                                    <strong>{track.artistName}</strong>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                    } */}
-
-                <Carousel interval={null} indicators={false} defaultActiveIndex={0} className="top-tracks-carousel">
+            </div>
+            <Carousel interval={null} indicators={false} defaultActiveIndex={0} className="top-tracks-carousel">
                     {carouselTracks.map((element, i) => (
                         <Carousel.Item key={i}>
                         <div className="group-tracks">{element}</div>
                         </Carousel.Item>
                     ))}
                 </Carousel>
-            </div>
         </div>
     )
 }
