9bc8c2f3d5145fc7ab7f85a707064f3b3f181ed6
Danica <danicadickinson@gmail.com>
2021-03-29 20:56:10 -0700

    Playlist modal save to spotify

--- frontend/src/PlaylistPage/PlaylistTitle/PlaylistTitle.js
+++ frontend/src/PlaylistPage/PlaylistTitle/PlaylistTitle.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './PlaylistTitle.css';
+import Modal from "react-bootstrap/Modal";
 
 import dog from './puppy-icon.jpg';
 
 const axios = require("axios");
 
+
 const PlaylistTitle = ({playlistName, playlistID, groupCode}) => {
+
+    const [SavedisOpen, setSavedIsOpen] = React.useState(false);
+    const showSavedModal = () => {
+         setSavedIsOpen(true);
+     };
+     const hideSavedModal = () => {
+         setSavedIsOpen(false);
+     };
 
     var refreshToken = useSelector((state) => state.refreshToken)
 
@@ -35,10 +45,46 @@ const PlaylistTitle = ({playlistName, playlistID, groupCode}) => {
                     <h2 className="white"> {playlistName} </h2>
                 </div>
                 {/* <h4 className="created-date"><strong>Created On: {createdDate.substr(0, 10)}</strong></h4> */}
-                <div className="btn btn-sm playlist-button" onClick={addToSpotify}>
+                <div className="btn btn-sm playlist-button" 
+                    onClick={() => {
+                        addToSpotify();
+                        showSavedModal();
+                    }}>
                     Save Playlist to Spotify
                 </div>
             </div>
+            <>
+        <Modal
+          className="modalcss"
+          show={SavedisOpen}
+          onHide={hideSavedModal}
+          centered
+        >
+          <Modal.Body className="in-modal">
+            <h5 className="modal-text modal-head">
+              <strong>Playlist Saved!</strong>
+            </h5>
+            <p>
+              <button
+                className="btn-in-modal leave-buttons"
+                onclick="location.href='https://open.spotify.com/'"    
+              >
+                Open Spotify Webplayer
+              </button>
+            </p>
+            <p>
+              <button
+                onClick={hideSavedModal}
+                className="btn-in-modal leave-buttons"
+              >
+                Back to overlap
+              </button>
+            </p>
+          </Modal.Body>
+        </Modal>
+      </>              
+
+
         </div>
     );
 }
