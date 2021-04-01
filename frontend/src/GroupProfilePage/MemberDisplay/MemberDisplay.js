2deef84e87557fcb8613686a5f6598b264c9c03e
Beatrice Tam <beatrice.6293@gmail.com>
2021-04-01 19:21:45 -0400

    copy to clipboard from member display (less wack)

--- frontend/src/GroupProfilePage/MemberDisplay/MemberDisplay.js
+++ frontend/src/GroupProfilePage/MemberDisplay/MemberDisplay.js
@@ -43,8 +43,6 @@ const MemberDisplay = ({groupUsers, groupCode, toCompare}) => {
           <div className="copy-code">
             <div id = "myCode">{groupCode}</div>
             <div onClick={()=>{copyCode(); setShow(true)}} className="copy-button">
-              {show ? <AlertCopy /> : null}
-              {show ? window.setTimeout(function(){setShow(false)}, 5000) : null}
               <svg width="32" height="34" viewBox="0 0 32 34" xmlns="http://www.w3.org/2000/svg" className = "clipboard">
                 <path 
                   d="M25.3333 2.83333H19.76C19.2 1.19 17.7333 0 16 0C14.2667 0 12.8 1.19 12.24 2.83333H6.66667C5.2 2.83333 4 4.10833 4 5.66667V28.3333C4 29.8917 5.2 31.1667 6.66667 31.1667H25.3333C26.8 31.1667 28 29.8917 28 28.3333V5.66667C28 4.10833 26.8 2.83333 25.3333 2.83333ZM16 2.83333C16.7333 2.83333 17.3333 3.47083 17.3333 4.25C17.3333 5.02917 16.7333 5.66667 16 5.66667C15.2667 5.66667 14.6667 5.02917 14.6667 4.25C14.6667 3.47083 15.2667 2.83333 16 2.83333ZM25.3333 28.3333H6.66667V5.66667H9.33333V9.91667H22.6667V5.66667H25.3333V28.3333Z" 
@@ -69,8 +67,11 @@ const MemberDisplay = ({groupUsers, groupCode, toCompare}) => {
             <h1 className="text"><strong>Members</strong></h1>
             {/*link to add members*/}
             <AddMember />
-            <br></br>            
-            
+            <br></br>
+            {show ? 
+              <AlertCopy /> 
+            : null}
+            {show ? window.setTimeout(function(){setShow(false)}, 1500) : null}         
         </div>
 
         <h1 className="compare-message"><strong>Click on a friend to see the overlap in your listening habits!</strong></h1>
@@ -100,9 +101,11 @@ const MemberDisplay = ({groupUsers, groupCode, toCompare}) => {
 
 function AlertCopy () {
     return(
-      <Alert  variant="success" className="copy-alert">
-        <p>Copied to clipboard!</p>
-      </Alert>
+      <div className="copy-alert">
+        <Alert  variant="success">
+          <p>Copied to clipboard!</p>
+        </Alert>
+      </div>
     )
 }
 
