e06bc483029655b06349acfd4f986bc3a9344bf6
Josiann Zhou <zhou.josi@gmail.com>
2021-03-31 22:12:46 -0400

    top genre bars

--- frontend/src/GroupProfilePage/TopGenres/TopGenres.js
+++ frontend/src/GroupProfilePage/TopGenres/TopGenres.js
@@ -3,6 +3,8 @@ import './TopGenres.css';
 
 export const TopGenres = ({groupUsers}) => {
     
+    let filler = 0;
+
     //collect group top genres
     let allGenres = [];
     groupUsers.map((user) => (
@@ -26,6 +28,14 @@ export const TopGenres = ({groupUsers}) => {
     })
     
     let top_genres = uniqueGenres.slice(0,5);
+    
+    let totalCounts = 0;
+    uniqueGenres.map((genre) => {
+        if (genre.count > (uniqueGenres[Math.round(uniqueGenres.length/4)].count))
+            totalCounts = totalCounts + genre.count
+    })
+
+    top_genres[0].count/totalCounts < 0.5 ? filler = 50 : filler = 0
 
     return (
         <div className="top-genres-root">
@@ -33,26 +43,36 @@ export const TopGenres = ({groupUsers}) => {
                 <h1 className="text"><strong>Top Genres</strong></h1>
                 <div className="under-bar"></div>
             </div>
-            <div className="genre-bubbles">
-                <div className="top-genre-1">
-                    <div className="bubble"></div>
-                    <h2><strong>{top_genres[0] ? top_genres[0].genre : 'Unable to find genre'}</strong></h2>
+            <div className="genre-bars">
+                <div className="genre_bar_div one">
+                    <div className="genre_bar one" style={{width: filler + (top_genres[0].count/totalCounts)*100 + "%"}}>
+                        <strong className="genre_name">{top_genres[0] ? top_genres[0].genre : 'Unable to find genre'}</strong>
+                    </div>                   
+                    <strong className="genre_percentage">{top_genres[0] ? ((top_genres[0].count/totalCounts)*100).toFixed(2) + "%" : 'Unable to find genre'}</strong>                    
                 </div>
-                <div className="top-genre-2">
-                    <div className="bubble"></div>
-                    <h2><strong>{top_genres[1] ? top_genres[1].genre : 'Unable to find genre'}</strong></h2>
+                <div className="genre_bar_div two">
+                    <div className="genre_bar two" style={{width: (filler + (top_genres[1].count/totalCounts)*100) + "%"}}>
+                        <strong className="genre_name">{top_genres[1] ? top_genres[1].genre : 'Unable to find genre'}</strong>
+                    </div>
+                    <strong className="genre_percentage">{top_genres[1] ? ((top_genres[1].count/totalCounts)*100).toFixed(2) + "%" : 'Unable to find genre'}</strong>
                 </div>
-                <div className="top-genre-3">
-                    <div className="bubble"></div>
-                    <h2><strong>{top_genres[2] ? top_genres[2].genre : 'Unable to find genre'}</strong></h2>
+                <div className="genre_bar_div three">
+                    <div className="genre_bar three" style={{width: (filler + (top_genres[2].count/totalCounts)*100) + "%"}}>
+                        <strong className="genre_name">{top_genres[2] ? top_genres[2].genre : 'Unable to find genre'}</strong>
+                    </div>
+                    <strong className="genre_percentage">{top_genres[2] ? ((top_genres[2].count/totalCounts)*100).toFixed(2) + "%" : 'Unable to find genre'}</strong>
                 </div>
-                <div className="top-genre-4">
-                    <div className="bubble"></div>
-                    <h2><strong>{top_genres[3] ? top_genres[3].genre : 'Unable to find genre'}</strong></h2>
+                <div className="genre_bar_div four">
+                    <div className="genre_bar four" style={{width: (filler + (top_genres[3].count/totalCounts)*100) + "%"}}>
+                        <strong className="genre_name">{top_genres[3] ? top_genres[3].genre : 'Unable to find genre'}</strong>
+                    </div>
+                    <strong className="genre_percentage">{top_genres[3] ? ((top_genres[3].count/totalCounts)*100).toFixed(2) + "%" : 'Unable to find genre'}</strong>
                 </div>
-                <div className="top-genre-5">
-                    <div className="bubble"></div>
-                    <h2><strong>{top_genres[4] ? top_genres[4].genre : 'Unable to find genre'}</strong></h2>
+                <div className="genre_bar_div five">
+                    <div className="genre_bar five" style={{width: (filler + (top_genres[4].count/totalCounts)*100) + "%"}}>
+                        <strong className="genre_name">{top_genres[4] ? top_genres[4].genre : 'Unable to find genre'}</strong>
+                    </div>
+                    <strong className="genre_percentage">{top_genres[4] ? ((top_genres[4].count/totalCounts)*100).toFixed(2) + "%" : 'Unable to find genre'}</strong>
                 </div>
             </div>
         </div>
