832dca1c4da2b460a18756fab4f4dc76008f546c
Kevin Peng <kevinpkf311@gmail.com>
2021-04-01 14:51:54 -0700

    Overlay screen fixes

--- frontend/src/ScreenOverlay/ScreenOverlay.js
+++ frontend/src/ScreenOverlay/ScreenOverlay.js
@@ -1,12 +1,28 @@
 import "./ScreenOverlay.css";
 import { Spinner } from "react-bootstrap";
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const ScreenOverlay = (props) => {
+  const [renderBack, setRenderBack] = React.useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRenderBack(true);
+    }, 3000);
+  }, []);
+
   return (
     <div className="overlay">
-      <h1 className="mr-4">{props.text}</h1>
-      <Spinner animation="border" />
+      <div className="overlay-row">
+        <h1 className="mr-4 overlay-text">{props.text}</h1>
+        <Spinner animation="border" />
+      </div>
+      {renderBack && (
+        <Link to="/" className="back-link">
+          Something went wrong? Take me back!
+        </Link>
+      )}
     </div>
   );
 };
