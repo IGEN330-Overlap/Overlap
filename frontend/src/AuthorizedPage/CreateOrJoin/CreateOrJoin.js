d08a354b0749afee736ed2db484e1469c9a0db0e
Beatrice Tam <beatrice.6293@gmail.com>
2021-04-02 00:20:46 -0400

    create or join component alerts

--- frontend/src/AuthorizedPage/CreateOrJoin/CreateOrJoin.js
+++ frontend/src/AuthorizedPage/CreateOrJoin/CreateOrJoin.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { updateGroupList } from '../../Redux/Actions.js';
 
 import Collapse from 'react-bootstrap/Collapse';
+import Alert from "react-bootstrap/Alert";
 
 import add_member from '../../GroupProfilePage/MemberDisplay/add-member.svg';
 import plus_sign from '../../GroupProfilePage/PlaylistCarousel/add.svg';
@@ -24,12 +25,23 @@ const CreateOrJoin = (props) => {
     const [group_code, setGroupCode] = useState('')
 
     const spotifyID = useSelector(state => state.userObject);
+
+    //show join alert when no code entered
+    const [showNoCodeAlert, setShowNoCodeAlert] = useState(false);
+
+    //show join alert when code is invale
+    const [showInvalidAlert, setShowInvalidAlert] = useState(false);
+
+    //show no group name alert
+    const [showNoNameAlert, setShowNoNameAlert] = useState(false);
+
     
     //Create new group on arrow click
     function createNewGroup(){
         input_name = document.getElementById("newGroupName").value
         if (input_name === "") {
-            console.log('No group name entered!')   
+            console.log('No group name entered!');
+            setShowNoNameAlert(true);   
         }
         else {
             console.log(input_name);
@@ -59,7 +71,8 @@ const CreateOrJoin = (props) => {
     function joinGroup(){
         input_code = document.getElementById("joinGroupCode").value.toUpperCase()
         if (input_code === "") {
-            console.log('No group code entered!')
+            console.log('No group code entered!');
+            setShowNoCodeAlert(true);
         }
         else {
             console.log(input_code)
@@ -79,7 +92,10 @@ const CreateOrJoin = (props) => {
                 })
                 .catch((err) => console.log(err));
             })
-            .catch((err) => console.log(err));         
+            .catch((err) => {
+                console.log(err);
+                setShowInvalidAlert(true);
+            });         
         }
         document.getElementById("joinGroupCode").value = ''
     }
@@ -125,14 +141,20 @@ const CreateOrJoin = (props) => {
                             viewBox="0 0 429.92093 429.92093" 
                             width="429.92093pt" 
                             xmlns="http://www.w3.org/2000/svg"
-                            className="arrow">
-                            
+                            className="arrow"
+                        >
                             <path d="m366.960938 62.960938c-83.949219-83.949219-220.054688-83.949219-304 0-83.949219 83.945312-83.949219 220.050781 0 304 83.945312 83.945312 220.050781 83.945312 304 0 83.945312-83.949219 83.945312-220.054688 0-304zm-50.300782 159.097656-92.5 92.5c-1.875 1.890625-4.4375 2.9375-7.101562 2.902344-2.652344-.011719-5.195313-1.050782-7.097656-2.902344-1.871094-1.867188-2.925782-4.402344-2.925782-7.046875s1.054688-5.183594 2.925782-7.050781l75.5-75.5h-200.5c-5.523438 0-10-4.476563-10-10 0-5.523438 4.476562-10 10-10h200.5l-75.5-75.5c-3.894532-3.894532-3.894532-10.207032 0-14.101563 3.894531-3.890625 10.207031-3.890625 14.097656 0l92.5 92.5c3.828125 3.945313 3.875 10.203125.101562 14.199219zm0 0"/>
-                            
                         </svg>
                     </div>
                 </div>
             </Collapse>
+            {showNoNameAlert ? 
+            <div className="create-error">
+                <AlertCreateJoin type="warning" message="No group name entered!"/>
+            </div>
+            : null
+            }
+            {showNoNameAlert ? <div className="timeout"> {window.setTimeout(function(){setShowNoNameAlert(false)}, 1500)} </div> : null}
 
             <div
                 onClick={() => setJoinOpen(!openJoin)}
@@ -155,12 +177,37 @@ const CreateOrJoin = (props) => {
                             
                             <path d="m366.960938 62.960938c-83.949219-83.949219-220.054688-83.949219-304 0-83.949219 83.945312-83.949219 220.050781 0 304 83.945312 83.945312 220.050781 83.945312 304 0 83.945312-83.949219 83.945312-220.054688 0-304zm-50.300782 159.097656-92.5 92.5c-1.875 1.890625-4.4375 2.9375-7.101562 2.902344-2.652344-.011719-5.195313-1.050782-7.097656-2.902344-1.871094-1.867188-2.925782-4.402344-2.925782-7.046875s1.054688-5.183594 2.925782-7.050781l75.5-75.5h-200.5c-5.523438 0-10-4.476563-10-10 0-5.523438 4.476562-10 10-10h200.5l-75.5-75.5c-3.894532-3.894532-3.894532-10.207032 0-14.101563 3.894531-3.890625 10.207031-3.890625 14.097656 0l92.5 92.5c3.828125 3.945313 3.875 10.203125.101562 14.199219zm0 0"/>
                             
-                        </svg></div>
+                        </svg>
+                    </div>
                 </div>
             </Collapse>
-
+            {showNoCodeAlert ? 
+            <div className="join-error">
+                <AlertCreateJoin type="warning" message="No group code entered!"/>
+            </div>
+            : null
+            }
+            {showNoCodeAlert ? <div className="timeout"> {window.setTimeout(function(){setShowNoCodeAlert(false)}, 1500)} </div> : null}
+            {showInvalidAlert ? 
+            <div className="join-error">
+                <AlertCreateJoin type="danger" message="Invalid group code!"/>
+            </div>
+            : null
+            }
+            {showInvalidAlert ? <div className="timeout"> {window.setTimeout(function(){setShowInvalidAlert(false)}, 1750)} </div> : null}
         </div>
     );
+}
+
+//join group error alert
+function AlertCreateJoin (props) {
+    return(
+      <div className="error-alert">
+        <Alert variant={props.type}>
+          <p>{props.message}</p>
+        </Alert>
+      </div>
+    )
 }
 
 export default CreateOrJoin;
--- frontend/src/GroupProfilePage/MemberDisplay/MemberDisplay.js
+++ frontend/src/GroupProfilePage/MemberDisplay/MemberDisplay.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import { render } from 'react-dom';
 import { useSelector } from 'react-redux';
 
 import './MemberDisplay.css';
