e47989a100c983f3e028dab35d155728ef81b1fd
Beatrice Tam <beatrice.6293@gmail.com>
2021-04-03 17:06:19 -0400

    page frame setup

--- frontend/src/HowItWorks/HowItWorks.js
+++ frontend/src/HowItWorks/HowItWorks.js
@@ -7,7 +7,38 @@ const HowItWorks = (props) => {
     return (
         <div className="hiw-root">
             <Navbar1 />
-            How it works.
+            <div className="hiw-info">
+                <div className="hiw-insights">
+                    <div className="hiw-content">
+                        <h1><strong>My Insights + Comparisons</strong></h1>
+                        <div className="under-bar"></div>
+                    </div>
+                </div>
+                <div className="hiw-playlistgeneration">
+                <div className="hiw-content">
+                        <h1><strong>Playlist Generation</strong></h1>
+                        <div className="under-bar"></div>
+                    </div>
+                </div>
+                <div className="hiw-genres">
+                    <div className="hiw-content">
+                        <h1><strong>Genres</strong></h1>
+                        <div className="under-bar"></div>
+                    </div>
+                </div>
+                <div className="hiw-topstats">
+                    <div className="hiw-content">
+                        <h1><strong>Tracks + Artists</strong></h1>
+                        <div className="under-bar"></div>
+                    </div>
+                </div>
+                <div className="hiw-musicstats">
+                    <div className="hiw-content">
+                        <h1><strong>Music Stats</strong></h1>
+                        <div className="under-bar"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
