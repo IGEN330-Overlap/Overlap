9062a08b164ffe01075f2aeb13760cfb426c44d3
Kevin Peng <kevinpkf@gmail.com>
2021-04-08 17:43:16 -0700

    About us fixes

--- frontend/src/AboutUs/BabyPics/BabyPics.js
+++ frontend/src/AboutUs/BabyPics/BabyPics.js
@@ -1,41 +1,52 @@
-import './BabyPics.css';
-import Brendan from './Brendan.png';
-import Kevin from './Kevin.jpg';
-import Beatrice from './Beatrice.jpg';
-import Josi from './Josi.jpg';
-import Danica from './Danica.jpg';
-import RealRemy from './RealRemy.jpg';
+import "./BabyPics.css";
+import Brendan from "./Brendan.png";
+import Kevin from "./Kevin.jpg";
+import Beatrice from "./Beatrice.jpg";
+import Josi from "./Josi.jpg";
+import Danica from "./Danica.jpg";
+import RealRemy from "./RealRemy.jpg";
 
 const Babies = (props) => {
-    return(
-        <div className= "baby-members">
-            
-            <div className= 'container-baby'>
-                <img className='icon-baby' src={Kevin} alt= 'Kevin'></img>
-                <p className="baby-name">
-                                    <b>Kevin</b></p></div>
-            <div className= 'container-baby'>
-                <img className='icon-baby' src={Danica} alt= 'Danica'></img>
-                <p className="baby-name">
-                                    <b>Danica</b></p></div>
-            <div className= 'container-baby'>
-                <img className='icon-baby' src={RealRemy} alt= 'RealRemy'></img>
-                <div className="baby-name">
-                                    <b>Remy</b></div></div>
-            <div className= 'container-baby'>
-                <img className='icon-baby' src={Josi} alt= 'Josi'></img>
-                <div className="baby-name">
-                                    <b>Josi</b></div></div>
-            <div className= 'container-baby'>
-                <img className='icon-baby' src={Brendan} alt= 'Brendan'></img>
-                <div className="baby-name">
-                                    <b>Brendan</b></div></div>
-            <div className= 'container-baby'>    
-                <img className='icon-baby' src={Beatrice} alt= 'Beatrice'></img>
-                <div className="baby-name">
-                                    <b>Beatrice</b></div></div>
+  return (
+    <div className="baby-members">
+      <div className="container-baby">
+        <img className="icon-baby" src={Kevin} alt="Kevin"></img>
+        <p className="baby-name">
+          <b>Kevin</b>
+        </p>
+      </div>
+      <div className="container-baby">
+        <img className="icon-baby" src={Danica} alt="Danica"></img>
+        <p className="baby-name">
+          <b>Danica</b>
+        </p>
+      </div>
+      <div className="container-baby">
+        <img className="icon-baby" src={RealRemy} alt="RealRemy"></img>
+        <div className="baby-name">
+          <b>Remy</b>
         </div>
-    )
-}
+      </div>
+      <div className="container-baby">
+        <img className="icon-baby" src={Josi} alt="Josi"></img>
+        <div className="baby-name">
+          <b>Josi</b>
+        </div>
+      </div>
+      <div className="container-baby">
+        <img className="icon-baby" src={Brendan} alt="Brendan"></img>
+        <div className="baby-name">
+          <b>Brendan</b>
+        </div>
+      </div>
+      <div className="container-baby">
+        <img className="icon-baby" src={Beatrice} alt="Beatrice"></img>
+        <div className="baby-name">
+          <b>Beatrice</b>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Babies;
--- frontend/src/AboutUs/TextBlurb/TextBlurb.js
+++ frontend/src/AboutUs/TextBlurb/TextBlurb.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import './TextBlurb.css';
+import React from "react";
+import "./TextBlurb.css";
+import { ReactComponent as GithubLogo } from "./github.svg";
 
 const Blurb = (props) => {
-    return(
+  return (
     <div className="about-name-container">
-        <h1 className="big-text"><strong>About us</strong></h1>
-        <div className="under-bar"></div>
-        <p className='sub-text'>Hey! Glad you made it. Overlap is a web application 
-        designed and created by six Integrated Engineering Students at UBC. 
-        One thing the COVID pandemic has showed us is the lack of socialibilty in music. 
-        Where you could previously go to parties or concerts and discover new music, 
-        the pandemic has limited the opportunities to expand your own horizons. 
-        </p> 
-        <p className='sub-text'>If you have any question, comments, or concerns, please reach out!</p>
+      <h1 className="big-text">
+        <strong>About us</strong>
+      </h1>
+      <div className="under-bar"></div>
+      <p className="sub-text">
+        Hey! Glad you made it. Overlap is a web application designed and created
+        by six Integrated Engineering Students at UBC. One thing the COVID
+        pandemic has showed us is the lack of socialibilty in music. Where you
+        could previously go to parties or concerts and discover new music, the
+        pandemic has limited the opportunities to expand your own horizons.
+      </p>
+      <p className="sub-text">
+        If you have any question, comments, or concerns, please reach out!
+      </p>
+      <a
+        className="sub-text d-flex flex-row align-items-center github-link"
+        href="https://github.com/IGEN330-Overlap/Overlap"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+        <GithubLogo className="github-logo"/>
+      </a>
     </div>
-    )
-}
+  );
+};
 
 export default Blurb;
