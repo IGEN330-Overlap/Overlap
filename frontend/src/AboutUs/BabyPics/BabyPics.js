b023e982e809414c971cf2bba3de5e5ca0b4f746
Josiann Zhou <zhou.josi@gmail.com>
2021-04-08 20:24:40 -0400

    added linkedin links on click of baby images

--- frontend/src/AboutUs/BabyPics/BabyPics.js
+++ frontend/src/AboutUs/BabyPics/BabyPics.js
@@ -10,30 +10,42 @@ const Babies = (props) => {
     return(
         <div className= "baby-members">
             
-            <div className= 'container-baby'>
+            <div className= 'container-baby' onClick={()=>window.open('https://www.linkedin.com/in/-kevin-peng/')}>
                 <img className='icon-baby' src={Kevin} alt= 'Kevin'></img>
                 <p className="baby-name">
-                                    <b>Kevin</b></p></div>
-            <div className= 'container-baby'>
+                    <b>Kevin</b>
+                </p>
+            </div>
+            <div className= 'container-baby' onClick={()=>window.open('https://www.linkedin.com/in/danica-dickinson-997137197/')}>
                 <img className='icon-baby' src={Danica} alt= 'Danica'></img>
                 <p className="baby-name">
-                                    <b>Danica</b></p></div>
-            <div className= 'container-baby'>
+                    <b>Danica</b>
+                </p>
+            </div>
+            <div className= 'container-baby' onClick={()=>window.open('https://www.linkedin.com/in/remy-zhang/')}>
                 <img className='icon-baby' src={RealRemy} alt= 'RealRemy'></img>
                 <div className="baby-name">
-                                    <b>Remy</b></div></div>
-            <div className= 'container-baby'>
-                <img className='icon-baby' src={Josi} alt= 'Josi'></img>
+                    <b>Remy</b>
+                </div>
+            </div>
+            <div className= 'container-baby' onClick={()=>window.open('https://www.linkedin.com/in/josiannzhou/')}>
+                <img className='icon-baby' src={Josi} alt= 'Josie'></img>
                 <div className="baby-name">
-                                    <b>Josie</b></div></div>
-            <div className= 'container-baby'>
+                    <b>Josie</b>
+                </div>
+            </div>
+            <div className= 'container-baby' onClick={()=>window.open('https://www.linkedin.com/in/brendan-lai-176460182/')}>
                 <img className='icon-baby' src={Brendan} alt= 'Brendan'></img>
                 <div className="baby-name">
-                                    <b>Brendan</b></div></div>
-            <div className= 'container-baby'>    
+                    <b>Brendan</b>
+                </div>
+            </div>
+            <div className= 'container-baby' onClick={()=>window.open('https://www.linkedin.com/in/beatrice-tam/')}>    
                 <img className='icon-baby' src={Beatrice} alt= 'Beatrice'></img>
                 <div className="baby-name">
-                                    <b>Beatrice</b></div></div>
+                    <b>Beatrice</b>
+                </div>
+            </div>
         </div>
     )
 }
